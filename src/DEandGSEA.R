929534db5bc13f94492c33bba8b2d03c572c7ef2
mishashe <52411283+mishashe@users.noreply.github.com>
2021-12-02 16:49:12 +0100

    Add files via upload

---
+++ src/DEandGSEA.R
@@ -0,0 +1,12 @@
+# export OPENBLAS_NUM_THREADS=1
+# export GOTO_NUM_THREADS=1
+# export OMP_NUM_THREADS=1
+
+
+# For github
+# cd /home/m.sheinman/Development/Krupakar
+# git add ./src/*.*
+# git commit --all -m "empty project"
+# git push origin master
+
+setwd("/home/m.sheinman/Development/Krupakar")
